b34220ff832d7ef68a1f86fcd5f3e5ce58f19b4e
Aya Ramirez <ayaramz@gmail.com>
2022-06-08 11:54:03 -0400

    commit tree 0b8c6527c91eabca4aff5fb4a190fa607bcd6bfd

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts greet("Jimmy")
+    return "Hello, #{name}!"
+end
+
+puts greet("Naureen")
+
+
+def greet_with_default(name = "programmer")
+    return "Hello, #{name}"
+end
+
+puts greet_with_default("Naureen")
+
+def add(a,b)
+    return a + b
+end
+
+puts add(1,2)
+
+def halve(a)
+    if a = "string" 
+        return nil
+    if a === integer || float
+        return a/2
+end
+
+halve(6)
